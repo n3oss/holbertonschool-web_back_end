3008c2d622124ba185577ed58dce511bae736cc4
root <bneoss07@gmail.com>
2026-04-18 22:50:08 +0000

    1

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python3
+"""basic caching system."""
 
 BaseCaching = __import__('base_caching').BaseCaching
 
 
 class BasicCache(BaseCaching):
+    """This class represents a basic cache without size limit."""
 
     def put(self, key, item):
+        """Store an item in the cache."""
         if key is None or item is None:
             return
         self.cache_data[key] = item
 
     def get(self, key):
+        """Retrieve an item from the cache by key."""
+        if key is None:
+            return None
+        return self.cache_data.get(key)
