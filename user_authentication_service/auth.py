cd4a6cedaf03daf552966a321a32a66c27e42719
root <bneoss07@gmail.com>
2026-04-22 12:47:02 +0000

    1

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -6,18 +6,6 @@ from db import DB
 from user import User
 from bcrypt import hashpw, gensalt
 from sqlalchemy.orm.exc import NoResultFound
-from sqlalchemy.orm.exc import NoResultFound
-
-
-def create_session(self, email: str) -> str:
-    """Create a session for the user and return the session ID."""
-    try:
-        user = self._db.find_user_by(email=email)
-    except NoResultFound:
-        return None
-    session_id = _generate_uuid()
-    self._db.update_user(user.id, session_id=session_id)
-    return session_id
 
 
 def _generate_uuid() -> str:
@@ -53,3 +41,13 @@ class Auth:
             return False
         return bcrypt.checkpw(password.encode("utf-8"),
                               user.hashed_password)
+
+    def create_session(self, email: str) -> str:
+        """Create a session for the user and return the session ID."""
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return None
+        session_id = _generate_uuid()
+        self._db.update_user(user.id, session_id=session_id)
+        return session_id
